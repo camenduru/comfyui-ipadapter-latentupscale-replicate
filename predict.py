db8c8949f61f174c3376ab4f4895a25788c79487
camenduru <54370274+camenduru@users.noreply.github.com>
2024-04-22 22:47:04 +0300

    test

--- predict.py
+++ predict.py
@@ -84,12 +84,21 @@ class Predictor(BasePredictor):
         red_part: Path = Input(description="Red part"),
         red_positive_prompt: str = Input(default="anime illustration of a young woman with a black jacket"),
         red_negative_prompt: str = Input(default=""),
+        red_threshold: float = Input(0.15),
+        red_image_weight: float = Input(0.7),
+        red_prompt_weight: float = Input(1.0),
         green_part: Path = Input(description="Green part"),
         green_positive_prompt: str = Input(default="illustration of a blond woman"),
         green_negative_prompt: str = Input(default="anime"),
+        green_threshold: float = Input(0.15),
+        green_image_weight: float = Input(0.7),
+        green_prompt_weight: float = Input(1.0),
         black_part: Path = Input(description="Black part"),
         black_positive_prompt: str = Input(default="closeup of two girl friends shopping in a sci-fi space station"),
         black_negative_prompt: str = Input(default="blurry, lowres, bad art, ill, distorted, malformed, horror"),
+        black_threshold: float = Input(0.15),
+        black_image_weight: float = Input(0.7),
+        black_prompt_weight: float = Input(1.0),
         color_mask: Path = Input(description="Color Mask"),
         seed: int = Input(543543),
         steps: int = Input(30),
@@ -112,7 +121,7 @@ class Predictor(BasePredictor):
             output2_image, output2_mask = nodes.LoadImage().load_image(str(green_part))
             output3_image, output3_mask = nodes.LoadImage().load_image(str(black_part))
             color_image, color_mask = nodes.LoadImage().load_image(str(color_mask))
-            red, green, blue, cyan, magenta, yellow, black, white = mask_from_colors(image=color_image, threshold_r=0.15, threshold_g=0.15, threshold_b=0.15, remove_isolated_pixels=0, fill_holes=False)
+            red, green, blue, cyan, magenta, yellow, black, white = mask_from_colors(image=color_image, threshold_r=red_threshold, threshold_g=green_threshold, threshold_b=black_threshold, remove_isolated_pixels=0, fill_holes=False)
 
             tokens_1 = self.clip.tokenize(red_positive_prompt)
             cond_1, pooled_1 = self.clip.encode_from_tokens(tokens_1, return_pooled=True)
@@ -120,7 +129,7 @@ class Predictor(BasePredictor):
             n_tokens_1 = self.clip.tokenize(red_negative_prompt)
             n_cond_1, n_pooled_1 = self.clip.encode_from_tokens(n_tokens_1, return_pooled=True)
             n_cond_1 = [[n_cond_1, {"pooled_output": n_pooled_1}]]
-            params_1, positive_1, negative_1 = IPAdapterPlus.IPAdapterRegionalConditioning().conditioning(output1_image, image_weight=0.7, prompt_weight=1.0, weight_type='linear', start_at=0.0, end_at=1.0, mask=red, positive=cond_1, negative=n_cond_1)
+            params_1, positive_1, negative_1 = IPAdapterPlus.IPAdapterRegionalConditioning().conditioning(output1_image, image_weight=red_image_weight, prompt_weight=red_prompt_weight, weight_type='linear', start_at=0.0, end_at=1.0, mask=red, positive=cond_1, negative=n_cond_1)
 
             tokens_2 = self.clip.tokenize(green_positive_prompt)
             cond_2, pooled_2 = self.clip.encode_from_tokens(tokens_2, return_pooled=True)
@@ -128,7 +137,7 @@ class Predictor(BasePredictor):
             n_tokens_2 = self.clip.tokenize(green_negative_prompt)
             n_cond_2, n_pooled_2 = self.clip.encode_from_tokens(n_tokens_2, return_pooled=True)
             n_cond_2 = [[n_cond_2, {"pooled_output": n_pooled_2}]]
-            params_2, positive_2, negative_2 = IPAdapterPlus.IPAdapterRegionalConditioning().conditioning(output2_image, image_weight=0.7, prompt_weight=1.0, weight_type='linear', start_at=0.0, end_at=1.0, mask=green, positive=cond_2, negative=n_cond_2)
+            params_2, positive_2, negative_2 = IPAdapterPlus.IPAdapterRegionalConditioning().conditioning(output2_image, image_weight=green_image_weight, prompt_weight=green_prompt_weight, weight_type='linear', start_at=0.0, end_at=1.0, mask=green, positive=cond_2, negative=n_cond_2)
 
             tokens_3 = self.clip.tokenize(black_positive_prompt)
             cond_3, pooled_3 = self.clip.encode_from_tokens(tokens_3, return_pooled=True)
@@ -136,7 +145,7 @@ class Predictor(BasePredictor):
             n_tokens_3 = self.clip.tokenize(black_negative_prompt)
             n_cond_3, n_pooled_3 = self.clip.encode_from_tokens(n_tokens_3, return_pooled=True)
             n_cond_3 = [[n_cond_3, {"pooled_output": n_pooled_3}]]
-            params_3, positive_3, negative_3 = IPAdapterPlus.IPAdapterRegionalConditioning().conditioning(output3_image, image_weight=0.7, prompt_weight=1.0, weight_type='linear', start_at=0.0, end_at=1.0, mask=black, positive=None, negative=None)
+            params_3, positive_3, negative_3 = IPAdapterPlus.IPAdapterRegionalConditioning().conditioning(output3_image, image_weight=black_image_weight, prompt_weight=black_prompt_weight, weight_type='linear', start_at=0.0, end_at=1.0, mask=black, positive=None, negative=None)
             positive = conditioning_combine_multiple(conditioning_1=positive_1, conditioning_2=positive_2, conditioning_3=cond_3)
             negative = conditioning_combine_multiple(conditioning_1=negative_1, conditioning_2=negative_2, conditioning_3=n_cond_3)
             ipadapter_params = IPAdapterPlus.IPAdapterCombineParams().combine(params_1=params_1, params_2=params_2, params_3=params_3)
